cbfb17e1a25b83f5586db0a54f1b7217fb6a8fab
klex <fanreds@wp.pl>
2012-11-01 15:12:38 +0100

    01-state

---
+++ skryptti.js
@@ -0,0 +1,17 @@
+<!--
+ <script type="text/javascript">
+    function PokazAkapit(nazwa)
+    {
+    var akapit = document.getElementById(nazwa);
+    var view = akapit.style.display;
+    if (view == "block")
+    {
+    akapit.style.display = "none";
+    }
+    if (view == "none")
+    {
+    akapit.style.display = "block";
+    }
+    }
+ </script>
+ //-->
